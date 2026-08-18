065db01e001e90802ba2b8b06dec5254f44709ed
brandoncyu <Brandoncyu@gmail.com>
2026-08-17 21:58:49 -0700

    fix

--- src/models/Theatre.js
+++ src/models/Theatre.js
@@ -1,5 +1,45 @@
+const db = require('../db')
 class Theatre {
-
+  constructor({id, name, address}) {
+    this._id = id
+    this._name = name
+    this._address = address
+  }
+  get id() {
+    return this._id
+  }
+  set id(val) {
+    throw new Error('CANNOT SET ID')
+  }
+  get removed() {
+    return false
+  }
+  set removed(id) {
+    throw new ERROR('CANNOT REMOVE ID MANUALLY')
+  }
+  static async all() {
+    let theatres = await db('theatres')
+    return theatres.map(element => new Theatre(element))
+  }
+  static async find(id) {
+    let theatres = await db('theatres')
+    let returnValue = theatres.find(element => element.id == id)
+    if (!returnValue) {
+      throw new ERROR('ID DOES NOT EXIST')
+    }
+    return new Theatre(returnValue)
+  }
+  async save(id) {
+    const theatres = await db('theatres')
+    if (!this.id) {
+      theatres.push()
+    } else {
+      const theatres = await db('theatres').where({id: this.id}).update({name: this.name, address: this.address}).returning('*')
+      console.log(theatres)
+      return new Theatre(theatres[0])
+    }
+    return this
+  }
 }
 
 module.exports = Theatre
--- test/models/theatre.test.js
+++ test/models/theatre.test.js
@@ -1,38 +1,38 @@
-const { expect } = require('chai')
+const {expect} = require('chai')
 const Theatre = require('../../src/models/theatre')
 
-describe('Theatre Model', function () {
+describe('Theatre Model', function() {
   beforeEach(() => global.knex.seed.run())
-  describe('get/set #id', function () {
-    it('should return the id when accessed', async function () {
-      const theatre = new Theatre({ id: 99, name: 'Name', address: 'Address' })
+  describe('get/set #id', function() {
+    it('should return the id when accessed', async function() {
+      const theatre = new Theatre({id: 99, name: 'Name', address: 'Address'})
 
       expect(theatre.id).to.equal(99)
     })
-    it('should throw an error if you try and set the id outside of the constructor', async function () {
-      const theatre = new Theatre({ id: 99, name: 'Name', address: 'Address' })
+    it('should throw an error if you try and set the id outside of the constructor', async function() {
+      const theatre = new Theatre({id: 99, name: 'Name', address: 'Address'})
       const actual = () => theatre.id = 100
 
       expect(actual).to.throw()
     })
   })
 
-  describe('get/set #removed', function () {
-    it('should return the removed attribute when accessed', function () {
-      const theatre = new Theatre({ name: 'Name', address: 'Address' })
+  describe('get/set #removed', function() {
+    it('should return the removed attribute when accessed', function() {
+      const theatre = new Theatre({name: 'Name', address: 'Address'})
 
       expect(theatre.removed).to.be.false
     })
-    it('should throw an error if you try and set the removed attribute manually', function () {
-      const theatre = new Theatre({ name: 'Name', address: 'Address' })
+    it('should throw an error if you try and set the removed attribute manually', function() {
+      const theatre = new Theatre({name: 'Name', address: 'Address'})
       const actual = () => theatre.removed = true
 
       expect(actual).to.throw()
     })
   })
 
-  describe('.all()', function () {
-    it('should return all rows in the theatre table as instances of the Theatre model', async function () {
+  describe('.all()', function() {
+    it('should return all rows in the theatre table as instances of the Theatre model', async function() {
       const theatres = await Theatre.all()
 
       expect(theatres.length).to.equal(3)
@@ -41,51 +41,46 @@ describe('Theatre Model', function () {
     })
   })
 
-  describe('.find()', function () {
-    it('should return the given theatre as a Theatre instance', async function () {
+  describe('.find()', function() {
+    it('should return the given theatre as a Theatre instance', async function() {
       const theatre = await Theatre.find(1)
 
       expect(theatre).to.be.ok
       expect(theatre.id).to.equal(1)
     })
 
-    it('should throw an error if the given theatre cannot be found', function () {
-      return Theatre.find(null)
-      .then(response => expect(response).to.not.be.ok)
-      .catch(error => expect(error).to.be.ok)
+    it('should throw an error if the given theatre cannot be found', function() {
+      return Theatre.find(null).then(response => expect(response).to.not.be.ok).catch(error => expect(error).to.be.ok)
     })
   })
 
-  describe('#save()' ,function () {
-    it('should create a new record if there is no id stored in the instance', async function () {
+  describe('#save()', function() {
+    it('should create a new record if there is no id stored in the instance', async function() {
       const name = `My Theatre`
       const address = `Theatre Address`
-      const theatre = new Theatre({ name, address })
+      const theatre = new Theatre({name, address})
 
       await theatre.save()
-      const row = await global.knex('theatres')
-        .limit(1)
-        .orderBy('id', 'desc')
-        .first()
+      const row = await global.knex('theatres').limit(1).orderBy('id', 'desc').first()
 
       expect(row.name).to.equal(name)
       expect(row.address).to.equal(address)
     })
 
-    it('should return the same instance from the save() invocation on creation', async function () {
+    it('should return the same instance from the save() invocation on creation', async function() {
       const name = `My Theatre`
       const address = `Theatre Address`
-      const theatre = new Theatre({ name, address })
+      const theatre = new Theatre({name, address})
 
       const result = await theatre.save()
 
       expect(result).to.equal(theatre)
     })
 
-    it('should return the same instance from with the newly created id on creation', async function () {
+    it('should return the same instance from with the newly created id on creation', async function() {
       const name = `My Theatre`
       const address = `Theatre Address`
-      const theatre = new Theatre({ name, address })
+      const theatre = new Theatre({name, address})
 
       const result = await theatre.save()
 
@@ -93,7 +88,7 @@ describe('Theatre Model', function () {
       expect(result.id).to.equal(theatre.id)
     })
 
-    it('should update the given resource if it already has an id', async function () {
+    it('should update the given resource if it already has an id', async function() {
       const theatre = await Theatre.find(1)
 
       const name = 'My New Name'
@@ -104,12 +99,12 @@ describe('Theatre Model', function () {
 
       await theatre.save()
 
-      const row = await global.knex('theatres').where({ id: 1 }).first()
+      const row = await global.knex('theatres').where({id: 1}).first()
       expect(row.name).to.equal(name)
       expect(row.address).to.equal(address)
     })
 
-    it('should return the same instance from the save() invocation on update', async function () {
+    it('should return the same instance from the save() invocation on update', async function() {
       const theatre = await Theatre.find(1)
 
       const name = 'My New Name'
@@ -123,42 +118,38 @@ describe('Theatre Model', function () {
       expect(result).to.equal(theatre)
     })
 
-    it('should throw an error if any of the values are missing on creation', function () {
+    it('should throw an error if any of the values are missing on creation', function() {
       const theatre = new Theatre()
 
-      return theatre.save()
-      .then(response => expect(response).to.not.be.ok)
-      .catch(error => expect(error).to.be.ok)
+      return theatre.save().then(response => expect(response).to.not.be.ok).catch(error => expect(error).to.be.ok)
     })
   })
 
-  describe('.destroy()', function () {
-    it('should delete the given theatre from the database', async function () {
+  describe('.destroy()', function() {
+    it('should delete the given theatre from the database', async function() {
       const theatre = await Theatre.find(1)
       await theatre.destroy()
 
-      const row = await global.knex('theatres').where({ id: 1 }).first()
+      const row = await global.knex('theatres').where({id: 1}).first()
       expect(row).to.be.undefined
     })
-    it('should return the same instance from the destroy() invocation', async function () {
+    it('should return the same instance from the destroy() invocation', async function() {
       const theatre = await Theatre.find(1)
       const result = await theatre.destroy()
 
       expect(theatre).to.deep.equal(result)
     })
-    it('should assign an internal property of `removed` to `true`', async function () {
+    it('should assign an internal property of `removed` to `true`', async function() {
       const theatre = await Theatre.find(1)
       const result = await theatre.destroy()
 
       expect(theatre.removed).to.be.true
     })
-    it('should throw an error if the theatre has already been removed', async function () {
+    it('should throw an error if the theatre has already been removed', async function() {
       const theatre = await Theatre.find(1)
       await theatre.destroy()
 
-      return theatre.destroy()
-      .then(response => expect(response).to.not.be.ok)
-      .catch(error => expect(error).to.be.ok)
+      return theatre.destroy().then(response => expect(response).to.not.be.ok).catch(error => expect(error).to.be.ok)
     })
   })
 })
